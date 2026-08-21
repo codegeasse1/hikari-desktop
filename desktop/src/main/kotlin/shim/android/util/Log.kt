c27a223fa0d731d762b0c743504040cc288b7bce
codegeasse <chattered199@gmail.com>
2026-08-21 19:35:05 +0530

    fix desktop compile errors

--- desktop/src/main/kotlin/shim/android/util/Log.kt
+++ desktop/src/main/kotlin/shim/android/util/Log.kt
@@ -5,6 +5,10 @@ object Log {
     fun v(tag: String, msg: String) = println("V/$tag: $msg")
     fun i(tag: String, msg: String) = println("I/$tag: $msg")
     fun w(tag: String, msg: String) = println("W/$tag: $msg")
+    fun w(tag: String, msg: String, t: Throwable?) {
+        println("W/$tag: $msg")
+        t?.printStackTrace()
+    }
     fun e(tag: String, msg: String) = System.err.println("E/$tag: $msg")
     fun e(tag: String, msg: String, t: Throwable?) {
         System.err.println("E/$tag: $msg")
