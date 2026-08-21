174177d355cba00350f90044f498e35a71b1218f
codegeasse <chattered199@gmail.com>
2026-08-21 19:26:07 +0530

    Add desktop/src/main/kotlin/shim/android/util/Log.kt

---
+++ desktop/src/main/kotlin/shim/android/util/Log.kt
@@ -0,0 +1,13 @@
+package android.util
+
+object Log {
+    fun d(tag: String, msg: String) = println("D/$tag: $msg")
+    fun v(tag: String, msg: String) = println("V/$tag: $msg")
+    fun i(tag: String, msg: String) = println("I/$tag: $msg")
+    fun w(tag: String, msg: String) = println("W/$tag: $msg")
+    fun e(tag: String, msg: String) = System.err.println("E/$tag: $msg")
+    fun e(tag: String, msg: String, t: Throwable?) {
+        System.err.println("E/$tag: $msg")
+        t?.printStackTrace()
+    }
+}
